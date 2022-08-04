ae3b3ee2fabfbe998428b14cf7810f904de5ca9c
Gregory Petrov <grishap96123@gmail.com>
2022-08-04 23:44:37 +0300

    removed unused

--- src/views/films/Films.js
+++ src/views/films/Films.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import Search from "./components/search/Search";
 import Body from "./components/body/Body";
