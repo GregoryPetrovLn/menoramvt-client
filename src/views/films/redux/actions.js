4c818e3784e68ee07010c0ebe0030b55f56e1df0
Gregory Petrov <grishap96123@gmail.com>
2022-08-07 00:52:31 +0300

    fixed url name

--- src/views/films/redux/actions.js
+++ src/views/films/redux/actions.js
@@ -5,7 +5,7 @@ export const getFilms = (title) => {
     return async (dispatch) => {
         try {
             dispatch({type: GET_FILMS.INIT})
-            const {data} = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/ombdb/films?s=${title}`)
+            const {data} = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/omdb/films?s=${title}`)
             dispatch({type: GET_FILMS.SUCCESS, payload: data})
         } catch (e) {
             dispatch({type: GET_FILMS.ERROR, payload: e.response.data.error})
@@ -17,7 +17,7 @@ export const getFilmById = (id) => {
     return async (dispatch) => {
         try {
             dispatch({type: GET_FILM_INFO.INIT})
-            const {data} = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/ombdb/films/${id}`)
+            const {data} = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/omdb/films/${id}`)
             dispatch({type: GET_FILM_INFO.SUCCESS, payload: data})
         } catch (e) {
             dispatch({type: GET_FILM_INFO.ERROR, payload: e.response.data.error})
